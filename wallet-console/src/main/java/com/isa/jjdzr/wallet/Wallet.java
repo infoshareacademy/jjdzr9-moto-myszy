cbc73d1fae9da7de6929b82bda766aa6cca5b17c
Michał Gomoła <michal.gomola.89@gmail.com>
2023-02-03 23:00:30 +0100

    Added fields to Wallet class

--- wallet-console/src/main/java/com/isa/jjdzr/wallet/Wallet.java
+++ wallet-console/src/main/java/com/isa/jjdzr/wallet/Wallet.java
@@ -1,4 +1,9 @@
 package com.isa.jjdzr.wallet;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
 public class Wallet {
+    private ArrayList<WalletAsset> wallet;
+    private BigDecimal cash;
 }
