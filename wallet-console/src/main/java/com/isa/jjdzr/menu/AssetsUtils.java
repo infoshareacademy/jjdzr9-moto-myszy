698887e67c1138d113b57b25c1bbc14576590c65
Michał Gomoła <michal.gomola.89@gmail.com>
2023-02-05 21:26:09 +0100

    added basic classes and methods skeleton

--- wallet-console/src/main/java/com/isa/jjdzr/menu/AssetsUtils.java
+++ wallet-console/src/main/java/com/isa/jjdzr/menu/AssetsUtils.java
@@ -1,2 +1,24 @@
-package com.isa.jjdzr.menu;public class AssetsUtils {
+package com.isa.jjdzr.menu;
+
+import com.isa.jjdzr.assets.Asset;
+import com.isa.jjdzr.wallet.WalletAsset;
+
+import java.util.ArrayList;
+
+public class AssetsUtils {
+    public void printAsset(Asset asset){
+
+    }
+
+    public void printWalletAsset(WalletAsset walletAsset) {
+
+    }
+
+    public void printAssetList(ArrayList<Asset> assetList) {
+
+    }
+
+    public void printWallet(ArrayList<WalletAsset> wallet) {
+
+    }
 }
