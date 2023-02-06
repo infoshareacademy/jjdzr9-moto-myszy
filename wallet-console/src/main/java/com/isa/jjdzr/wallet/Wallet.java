27b7bc944f07932dbbb5c437fe28918ae9ad895b
Michał Gomoła <michal.gomola.89@gmail.com>
2023-02-06 20:57:30 +0100

    wallet methods added

--- wallet-console/src/main/java/com/isa/jjdzr/wallet/Wallet.java
+++ wallet-console/src/main/java/com/isa/jjdzr/wallet/Wallet.java
@@ -6,4 +6,33 @@ import java.util.ArrayList;
 public class Wallet {
     private ArrayList<WalletAsset> wallet;
     private BigDecimal cash;
+
+    public Wallet() {
+        this.wallet = new ArrayList<>();
+        this.cash = new BigDecimal(0);
+    }
+
+    public void addAsset(WalletAsset asset) {
+        wallet.add(asset);
+    }
+
+    public void addCash(String cash) {
+        this.cash = this.cash.add(new BigDecimal(cash));
+    }
+
+    public void addCash(BigDecimal cash) {
+        this.cash = this.cash.add(cash);
+    }
+
+    public void spendCash(BigDecimal cash) {
+        this.cash = this.cash.subtract(cash);
+    }
+
+    public ArrayList<WalletAsset> getWallet() {
+        return wallet;
+    }
+
+    public BigDecimal getCash() {
+        return cash;
+    }
 }
