a712ad1d151fa4b794a388daad2a59f7fb21b5d4
Michał Gomoła <michal.gomola.89@gmail.com>
2023-02-06 21:35:00 +0100

    walletGenerator skeleton added

--- wallet-console/src/main/java/com/isa/jjdzr/assets/AssetsList.java
+++ wallet-console/src/main/java/com/isa/jjdzr/assets/AssetsList.java
@@ -5,7 +5,8 @@ import java.util.ArrayList;
 public class AssetsList {
     private ArrayList<Asset> assetsList;
 
-    //metoda generująca listę
+    //metoda generująca listę z
+    //metoda aktualizująca ceny o +/- 5% (z wykorzystaniem randoma)
 
 
 }
---
+++ wallet-console/src/main/java/com/isa/jjdzr/menu/WalletGenerator.java
@@ -0,0 +1,18 @@
+package com.isa.jjdzr.menu;
+
+import com.isa.jjdzr.wallet.Wallet;
+
+public class WalletGenerator {
+    public Wallet generateWallet() {
+        // welcome message
+        // make new wallet
+        // ask if want to add cash
+        // get cash amount
+        // return wallet
+        return null;
+    }
+
+    private String getCashAmount() {
+        return null;
+    }
+}
