df056c5c0c89524ba19b51200cc5f3322f4556a3
Michał Gomoła <michal.gomola.89@gmail.com>
2023-02-07 20:17:07 +0100

    walletGenerator class added

--- wallet-console/src/main/java/com/isa/jjdzr/consolePresentation/WalletGenerator.java
+++ wallet-console/src/main/java/com/isa/jjdzr/consolePresentation/WalletGenerator.java
@@ -2,17 +2,33 @@ package com.isa.jjdzr.consolePresentation;
 
 import com.isa.jjdzr.dto.Wallet;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 class WalletGenerator {
     public Wallet generateWallet() {
-        // welcome message
-        // make new wallet
-        // ask if want to add cash
-        // get cash amount
-        // return wallet
-        return null;
+
+        System.out.println("Utworzymy teraz nowy portfel inwestycyjny.");
+        Wallet wallet = new Wallet();
+        wallet.addCash(getCashAmount());
+        return wallet;
     }
 
     private String getCashAmount() {
-        return null;
+        Scanner scan = new Scanner(System.in);
+        String cash;
+        System.out.println("Podaj jaką kwotą chcesz zasilić portfel: ");
+        cash = scan.nextLine();
+        while (isInvalid(cash)) {
+            System.out.println("Nieprawidłowa wartość. Podaj kwotę: ");
+            cash = scan.nextLine();
+        }
+
+        return cash;
+    }
+
+    private boolean isInvalid(String str) {
+        String validSymbols = "[0-9.]*";
+        return !Pattern.matches(validSymbols, str);
     }
 }
