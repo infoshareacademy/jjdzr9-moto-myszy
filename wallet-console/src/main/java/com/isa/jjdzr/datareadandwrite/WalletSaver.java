34d127036e19f8ce934aea553bd03c415c77d8a4
Michał Gomoła <michal.gomola.89@gmail.com>
2023-02-15 22:16:36 +0100

    Wallet saver updated

--- wallet-console/src/main/java/com/isa/jjdzr/datareadandwrite/WalletSaver.java
+++ wallet-console/src/main/java/com/isa/jjdzr/datareadandwrite/WalletSaver.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.isa.jjdzr.dto.Wallet;
 import com.isa.jjdzr.dto.WalletAsset;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -27,7 +28,13 @@ public class WalletSaver {
         try {
             Files.writeString(path, s);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            File file = new File(path.toUri());
+            try {
+                file.createNewFile();
+                Files.writeString(path, s);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
         }
 
     }
