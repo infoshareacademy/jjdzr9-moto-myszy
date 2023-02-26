476351afc9449c95984ca541b46ba1661a1a12b9
Michał Gomoła <michal.gomola.89@gmail.com>
2023-02-26 19:48:59 +0100

    spring boot project added

---
+++ wallet-web/src/main/java/com/isa/jjdzr/walletweb/WalletWebApplication.java
@@ -0,0 +1,13 @@
+package com.isa.jjdzr.walletweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletWebApplication.class, args);
+	}
+
+}
