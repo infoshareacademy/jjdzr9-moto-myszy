3eece5c10fb27e4811b322879679b3a21425b18b
Michał Gomoła <michal.gomola.89@gmail.com>
2023-03-05 13:14:52 +0100

    basic User classes added

---
+++ wallet-console/src/main/java/com/isa/jjdzr/user/User.java
@@ -0,0 +1,9 @@
+package com.isa.jjdzr.user;
+
+import java.util.List;
+
+public class User {
+    private String username;
+    private String password;
+    private List<String> wallets;
+}
---
+++ wallet-console/src/main/java/com/isa/jjdzr/user/Users.java
@@ -0,0 +1,7 @@
+package com.isa.jjdzr.user;
+
+import java.util.List;
+
+public class Users {
+    private List<User> userList;
+}
