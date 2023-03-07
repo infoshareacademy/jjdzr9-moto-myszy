aa808f6ac1b69ae5896a4ac8843f7c8b11de1571
Michał Gomoła <michal.gomola.89@gmail.com>
2023-03-07 21:57:00 +0100

    base UserService class ready

--- wallet-web/src/main/java/com/isa/jjdzr/walletweb/UserService.java
+++ wallet-web/src/main/java/com/isa/jjdzr/walletweb/UserService.java
@@ -1,4 +1,15 @@
 package com.isa.jjdzr.walletweb;
 
+import java.util.List;
+
 public class UserService {
+    private List<User> users;
+
+    public User login(User user){
+        return new User();
+    }
+
+    public void addUser(User user){
+        users.add(user);
+    }
 }
