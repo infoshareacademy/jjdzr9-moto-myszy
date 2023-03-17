00304eced8672955620fbe0b1dcdbbd94b82c985
Łukasz Groth <great91g@gmail.com>
2023-03-17 10:59:26 +0100

    searching reparing

--- wallet-web/src/main/java/com/isa/jjdzr/walletweb/Controller.java
+++ wallet-web/src/main/java/com/isa/jjdzr/walletweb/Controller.java
@@ -8,7 +8,11 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @org.springframework.stereotype.Controller
 @RequiredArgsConstructor
@@ -47,5 +51,11 @@ public class Controller {
         redirectAttributes.addFlashAttribute("status", status);
         return "redirect:/login";
     }
+    @GetMapping("/search")
+    public List<String> search(@RequestParam("query") String query) {
+        List<String> results = new ArrayList<>();
+        return results;
+    }
 
 }
+
