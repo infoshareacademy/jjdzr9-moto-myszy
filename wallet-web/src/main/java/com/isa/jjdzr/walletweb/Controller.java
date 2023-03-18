06f9e2634a050ddcb7e410f5327c43fc4ab69e9c
Łukasz Groth <great91g@gmail.com>
2023-03-18 14:32:24 +0100

    animated logo

--- wallet-web/src/main/java/com/isa/jjdzr/walletweb/Controller.java
+++ wallet-web/src/main/java/com/isa/jjdzr/walletweb/Controller.java
@@ -8,11 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @org.springframework.stereotype.Controller
 @RequiredArgsConstructor
@@ -51,11 +47,5 @@ public class Controller {
         redirectAttributes.addFlashAttribute("status", status);
         return "redirect:/login";
     }
-    @GetMapping("/search")
-    public List<String> search(@RequestParam("query") String query) {
-        List<String> results = new ArrayList<>();
-        return results;
-    }
-
 }
 
