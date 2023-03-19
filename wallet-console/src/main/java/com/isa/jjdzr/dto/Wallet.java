672e3d465804fa4717e08332c91741a8177cb045
Michał Gomoła <michal.gomola.89@gmail.com>
2023-03-19 19:02:38 +0100

    wallet class refactored

--- wallet-console/src/main/java/com/isa/jjdzr/dto/Wallet.java
+++ wallet-console/src/main/java/com/isa/jjdzr/dto/Wallet.java
@@ -1,20 +1,17 @@
 package com.isa.jjdzr.dto;
 
 import com.isa.jjdzr.common.BaseEntity;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Wallet implements BaseEntity {
     private Long id;
     private Long userId;
     private String walletName;
     private BigDecimal cash;
-
-    public Wallet() {
-
-    }
-
 }
