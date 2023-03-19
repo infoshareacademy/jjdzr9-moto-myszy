3689aed2a7a49deb2f9a44b7caa216e985bf8c7e
Michał Gomoła <michal.gomola.89@gmail.com>
2023-03-19 19:03:31 +0100

    WalletAsset class refactored

--- wallet-console/src/main/java/com/isa/jjdzr/dto/WalletAsset.java
+++ wallet-console/src/main/java/com/isa/jjdzr/dto/WalletAsset.java
@@ -1,12 +1,14 @@
 package com.isa.jjdzr.dto;
 
 import com.isa.jjdzr.common.BaseEntity;
-import com.isa.jjdzr.market.Market;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
-import java.util.List;
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class WalletAsset implements BaseEntity {
     private Long id;
     private Long walletId;
