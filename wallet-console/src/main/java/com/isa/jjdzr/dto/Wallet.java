a181a4966d37cca6d76996fcd9619a3b0301511f
Michał Gomoła <michal.gomola.89@gmail.com>
2023-03-19 19:00:57 +0100

    walletgenerator deleted

--- wallet-console/src/main/java/com/isa/jjdzr/dto/Wallet.java
+++ wallet-console/src/main/java/com/isa/jjdzr/dto/Wallet.java
@@ -11,35 +11,10 @@ public class Wallet implements BaseEntity {
     private Long id;
     private Long userId;
     private String walletName;
-    private List<WalletAsset> wallet;
     private BigDecimal cash;
 
     public Wallet() {
-        this.wallet = new ArrayList<>();
-        this.cash = new BigDecimal(0);
-    }
-    //TODO: put those in wallet service
-    public void addAsset(WalletAsset asset) {
-        wallet.add(asset);
+
     }
 
-    public void addCash(String cash) {
-        this.cash = this.cash.add(new BigDecimal(cash));
-    }
-
-    public void addCash(BigDecimal cash) {
-        this.cash = this.cash.add(cash);
-    }
-
-    public void spendCash(BigDecimal cash) {
-        this.cash = this.cash.subtract(cash);
-    }
-
-    public List<WalletAsset> getWallet() {
-        return wallet;
-    }
-
-    public BigDecimal getCash() {
-        return cash;
-    }
 }
