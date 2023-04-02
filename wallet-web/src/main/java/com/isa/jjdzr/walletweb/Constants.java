d5af2d8295a84b7da65e760213cba14a187a6d97
Michał Gomoła <michal.gomola.89@gmail.com>
2023-04-02 20:33:11 +0200

    buying asset done

--- wallet-web/src/main/java/com/isa/jjdzr/walletweb/Constants.java
+++ wallet-web/src/main/java/com/isa/jjdzr/walletweb/Constants.java
@@ -10,4 +10,6 @@ public class Constants {
     public static final String TOP_UP_SUCCESS = "top_up_success";
     public static final String NOT_SUFFICIENT_QUANTITY_IN_WALLET = "not_sufficient_quantity";
     public static final String SELL_POSSIBLE = "sell_possible";
+    public static final String NOT_ENOUGH_MONEY = "not_enough_money";
+    public static final String BUY_POSSIBLE = "buy_possible";
 }
