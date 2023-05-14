08ebb97c0a1e8dee4c8d63c285e7a53111c53acd
Łukasz Groth <great91g@gmail.com>
2023-05-14 15:14:13 +0200

    BuyInfoDtoTest added

---
+++ wallet-web/src/test/java/com/isa/jjdzr/walletweb/dto/BuyInfoDtoTest.java
@@ -0,0 +1,15 @@
+package com.isa.jjdzr.walletweb.dto;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+public class BuyInfoDtoTest {
+
+
+}
