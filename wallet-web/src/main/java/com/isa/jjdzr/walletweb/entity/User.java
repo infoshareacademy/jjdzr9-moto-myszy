5fc345ab5130b327392ad1589e7dfb65dd456eb4
Michał Gomoła <michal.gomola.89@gmail.com>
2023-06-01 20:24:57 +0200

    added User Entity

---
+++ wallet-web/src/main/java/com/isa/jjdzr/walletweb/entity/User.java
@@ -0,0 +1,24 @@
+package com.isa.jjdzr.walletweb.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+}
--- wallet-web/src/main/java/com/isa/jjdzr/walletweb/repository/FileUserRepository.java
+++ wallet-web/src/main/java/com/isa/jjdzr/walletweb/repository/FileUserRepository.java
@@ -1,9 +1,9 @@
 package com.isa.jjdzr.walletweb.repository;
 
 import com.isa.jjdzr.walletcore.common.FileRepository;
-import com.isa.jjdzr.walletweb.dto.User;
+import com.isa.jjdzr.walletweb.dto.UserDto;
 import org.springframework.stereotype.Component;
 @Component
-public class FileUserRepository extends FileRepository<User> implements UserRepository{
+public class FileUserRepository extends FileRepository<UserDto> implements UserRepository{
 
 }
