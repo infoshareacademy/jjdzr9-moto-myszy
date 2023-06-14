8607cf948438cdbcf6974950bb3f866c94f856c5
Łukasz Groth <great91g@gmail.com>
2023-06-14 11:01:47 +0200

    NbpApiServiceImplTest added

--- wallet-web/src/test/java/com/isa/jjdzr/walletweb/service/NbpApiServiceImplTest.java
+++ wallet-web/src/test/java/com/isa/jjdzr/walletweb/service/NbpApiServiceImplTest.java
@@ -1,4 +1,26 @@
 package com.isa.jjdzr.walletweb.service;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import java.util.Map;
+import static org.junit.Assert.*;
 
 public class NbpApiServiceImplTest {
+    @Test
+    public void testGetExchangeRates() {
+        // Given
+        NbpApiService nbpApiService = new NbpApiServiceImpl();
+
+        // When
+        List<Map<String, Object>> exchangeRates = nbpApiService.getExchangeRates();
+
+        // Then
+        assertNotNull(exchangeRates);
+        assertFalse(exchangeRates.isEmpty());
+
+        for (Map<String, Object> rateMap : exchangeRates) {
+            assertTrue(rateMap.containsKey("currency"));
+            assertTrue(rateMap.containsKey("code"));
+            assertTrue(rateMap.containsKey("mid"));
+        }
+    }
 }
