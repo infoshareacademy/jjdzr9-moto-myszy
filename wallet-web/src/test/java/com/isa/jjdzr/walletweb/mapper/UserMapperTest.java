1d81e4346fbcef84b8160cf8a735c817b4ea6fa3
Łukasz Groth <great91g@gmail.com>
2023-06-16 13:18:02 +0200

    UserMapperTest added

---
+++ wallet-web/src/test/java/com/isa/jjdzr/walletweb/mapper/UserMapperTest.java
@@ -0,0 +1,38 @@
+package com.isa.jjdzr.walletweb.mapper;
+import com.isa.jjdzr.walletweb.dto.UserDto;
+import com.isa.jjdzr.walletweb.entity.UserEntity;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UserMapperTest {
+
+    @Test
+    public void testDtoToEntity() {
+
+        UserDto userDto = new UserDto();
+        userDto.setId(1L);
+        userDto.setUsername("john_doe");
+
+        UserMapper userMapper = UserMapper.MAPPER;
+
+        UserEntity userEntity = userMapper.dtoToEntity(userDto);
+
+        Assertions.assertEquals(userDto.getId(), userEntity.getId());
+        Assertions.assertEquals(userDto.getUsername(), userEntity.getUsername());
+    }
+
+    @Test
+    public void testEntityToDto() {
+
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(1L);
+        userEntity.setUsername("john_doe");
+
+        UserMapper userMapper = UserMapper.MAPPER;
+
+        UserDto userDto = userMapper.entityToDto(userEntity);
+
+        Assertions.assertEquals(userEntity.getId(), userDto.getId());
+        Assertions.assertEquals(userEntity.getUsername(), userDto.getUsername());
+    }
+}
